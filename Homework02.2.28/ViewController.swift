cfc2ea74a15cd9d189d6bf69096e248d0ba6eb50
Daniil Lebedev <disgarmonvideo@gmail.com>
2022-06-21 18:00:40 +0300

    Fin after lesson

--- Homework02.2.28/ViewController.swift
+++ Homework02.2.28/ViewController.swift
@@ -7,24 +7,58 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
+    
+    enum CurrentLight{
+        case red, yellow, green
+    }
 
     @IBOutlet var redLightView: UIView!
     @IBOutlet var yellowLightView: UIView!
     @IBOutlet var greenLightView: UIView!
+    
     @IBOutlet var starAndNextButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOff: CGFloat = 0.3
+    private let lightIsOn: CGFloat = 1
     
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLightView.layer.cornerRadius = redLightView.frame.size.width/2
-       // yellowLightView.layer.cornerRadius = 50
-       // greenLightView.layer.cornerRadius = 50
+        starAndNextButton.layer.cornerRadius = 10
+        
+        redLightView.alpha = lightIsOff
+        yellowLightView.alpha = lightIsOff
+        greenLightView.alpha = lightIsOff
+        
+        redLightView.layer.cornerRadius = redLightView.frame.height / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.height / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.height / 2
     }
 
     @IBAction func startAndNextButtonTapped() {
+        if starAndNextButton.currentTitle == "Start"{
+            starAndNextButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight{
+        case .red:
+            greenLightView.alpha = lightIsOff
+            redLightView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLightView.alpha = lightIsOff
+            yellowLightView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLightView.alpha = lightIsOff
+            greenLightView.alpha = lightIsOn
+            currentLight = .red
+        }
     }
-    
 }
 
